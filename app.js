cf06de6453f8210d2b964fdd376640ceae69f0b8
Aaron <ayybuddyy@gmail.com>
2019-09-22 02:43:52 -0400

    Feat and Refactor: Delete and separate handlers
    Add delete route but with no logic. Separate handler functions from
    the routes to make it cleaner and export the handlers if needed
    for future use.

--- app.js
+++ app.js
@@ -7,21 +7,11 @@ const app = express();
 // middleware: function that can modify incoming request data
 app.use(express.json());
 
-// app.get('/', (req, res) => {
-//   res
-//     .status(200)
-//     .json({ message: 'Hello from the server side', app: 'Natours' });
-// });
-
-// app.post('/', (req, res) => {
-//   res.send('You can post to this endpoint...');
-// });
-
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
@@ -31,9 +21,9 @@ app.get('/api/v1/tours', (req, res) => {
       tours: tours
     }
   });
-});
+};
 
-app.get('/api/v1/tours/:id', (req, res) => {
+const getTour = (req, res) => {
   // variables in url are called parameters
   console.log(req.params);
 
@@ -57,9 +47,9 @@ app.get('/api/v1/tours/:id', (req, res) => {
       tour
     }
   });
-});
+};
 
-app.post('/api/v1/tours', (req, res) => {
+const createTour = (req, res) => {
   //   console.log(req.body);
 
   const newId = tours[tours.length - 1].id + 1;
@@ -80,9 +70,9 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
-});
+};
 
-app.patch('/api/v1/tours/:id', (req, res) => {
+const updateTour = (req, res) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'fail',
@@ -95,7 +85,39 @@ app.patch('/api/v1/tours/:id', (req, res) => {
       tour: 'Updated tour here...'
     }
   });
-});
+};
+
+const deleteTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    });
+  }
+  // 204 is no content, delete code
+  // return data of null because resource no longer exists
+  res.status(204).json({
+    status: 'success',
+    data: null
+  });
+};
+
+// app.get('/api/v1/tours', getAllTours);
+// app.post('/api/v1/tours', createTour);
+// app.get('/api/v1/tours/:id', getTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
+
+app
+  .route('/api/v1/tours')
+  .get(getAllTours)
+  .post(createTour);
+
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 
 const port = 3000;
 
