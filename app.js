2f21d8e80d78826cd7c48737a5e68a89c661b228
Aaron <ayybuddyy@gmail.com>
2019-09-22 19:34:51 -0400

    Feat: Environment variables
    Set 2 npm scripts, one for production and one for development. Install
    dotenv. Create dotenv file.

--- app.js
+++ app.js
@@ -13,8 +13,12 @@ const app = express();
 // How we use middleware
 // express.json() is our middleware
 // middleware: function that can modify incoming request data
-app.use(morgan('dev'));
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware');
