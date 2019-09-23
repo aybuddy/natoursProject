d2c6165d141c50d75faa7dbd56478fd9d7140eb7
Aaron <ayybuddyy@gmail.com>
2019-09-23 01:33:11 -0400

    Feat: Connect to mongodb, install mongoose
    Set up Mongo Compass, shell, and atlas. Install mongoose and set
    up connection to atlas.

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DB_PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('DB connection successful'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
