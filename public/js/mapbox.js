7b817ce640423861dafb73101f4dee74faafb31e
Aaron <ayybuddyy@gmail.com>
2019-10-07 14:35:43 -0400

    Feat: Client-side bundling, alert messages
    Install parcel for client-side bundling. Create index for entry and
    bundle for output. Alerts.js for custom alerts. Export functions
    and import them into index.

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,53 +1,50 @@
 /* eslint-disable */
+export const displayMap = locations => {
+  mapboxgl.accessToken =
+    'pk.eyJ1IjoiYXlidWRkeSIsImEiOiJjazFmeHZ3ZXIwdW83M2hwN2wxbDl6cXc1In0.WNybvv_GiC232d1Dzl6LiQ';
 
-console.log('Hello from the client side!');
-const locations = JSON.parse(document.getElementById('map').dataset.locations);
-console.log(locations);
+  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/aybuddy/ck1fy4y9243lp1co4p70fs0qk',
+    scrollZoom: false
+    // center: [-118.113491, 34.111745],
+    // zoom: 7,
+    // interactive: false
+  });
 
-mapboxgl.accessToken =
-  'pk.eyJ1IjoiYXlidWRkeSIsImEiOiJjazFmeHZ3ZXIwdW83M2hwN2wxbDl6cXc1In0.WNybvv_GiC232d1Dzl6LiQ';
+  const bounds = new mapboxgl.LngLatBounds();
 
-var map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/aybuddy/ck1fy4y9243lp1co4p70fs0qk',
-  scrollZoom: false
-  // center: [-118.113491, 34.111745],
-  // zoom: 7,
-  // interactive: false
-});
+  locations.forEach(loc => {
+    // Create marker
+    const el = document.createElement('div');
+    el.className = 'marker';
 
-const bounds = new mapboxgl.LngLatBounds();
+    // Add marker
+    new mapboxgl.Marker({
+      element: el,
+      anchor: 'bottom'
+    })
+      .setLngLat(loc.coordinates)
+      .addTo(map);
 
-locations.forEach(loc => {
-  // Create marker
-  const el = document.createElement('div');
-  el.className = 'marker';
+    // Add popup
+    new mapboxgl.Popup({
+      offset: 30
+    })
+      .setLngLat(loc.coordinates)
+      .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+      .addTo(map);
 
-  // Add marker
-  new mapboxgl.Marker({
-    element: el,
-    anchor: 'bottom'
-  })
-    .setLngLat(loc.coordinates)
-    .addTo(map);
+    // Extend map bounds to include current location
+    bounds.extend(loc.coordinates);
+  });
 
-  // Add popup
-  new mapboxgl.Popup({
-    offset: 30
-  })
-    .setLngLat(loc.coordinates)
-    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
-    .addTo(map);
-
-  // Extend map bounds to include current location
-  bounds.extend(loc.coordinates);
-});
-
-map.fitBounds(bounds, {
-  padding: {
-    top: 200,
-    bottom: 150,
-    left: 100,
-    right: 100
-  }
-});
+  map.fitBounds(bounds, {
+    padding: {
+      top: 200,
+      bottom: 150,
+      left: 100,
+      right: 100
+    }
+  });
+};
