85b63bb78d842fa0cf3e17e52da1e3c4868b8076
Aaron <ayybuddyy@gmail.com>
2019-10-07 01:19:59 -0400

    Feat: mapbox
    Add client side js files. Integrate mapbox and grab location data
    from database. Add locations to mapbox and display on tour page.

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -24,7 +24,7 @@ exports.getTour = catchAsync(async (req, res, next) => {
 
   // 3. Render template using data from step 1
   res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour',
+    title: `${tour.name} Tour`,
     tour
   });
 });
