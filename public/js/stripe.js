6c81233a2a83c939652de4d2f6154807f26935be
Aaron <ayybuddyy@gmail.com>
2019-10-09 21:09:35 -0400

    Prepare app for deployment

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -6,9 +6,7 @@ export const bookTour = async tourId => {
   try {
     const stripe = Stripe('pk_test_OP6YJHtOnOYb2XBZv3FlSTsc00yVwdtcW4');
     // 1. Get checkout session from API
-    const session = await axios(
-      `http://localhost:3000/api/v1/bookings/checkout-session/${tourId}`
-    );
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
     // console.log(session);
 
     // 2. Create checkout form + charge the credit card
